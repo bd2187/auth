e160b85424aa0a69b64b7c15c1fa1506692b41fe
bd2187 <brandon.dionisio.26@gmail.com>
2019-09-09 21:38:05 -0700

    Initial commit. Create schema for authenticating email and password

---
+++ utils/userValidation.js
@@ -0,0 +1,20 @@
+const Joi = require("@hapi/joi");
+
+const schemas = {
+    authSchema: Joi.object().keys({
+        email: Joi.string()
+            .email({ minDomainSegments: 2 })
+            .required(),
+        password: Joi.string()
+            .regex(/^[a-zA-Z0-9]{3,30}$/)
+            .required()
+    })
+};
+
+module.exports = {
+    validateBody: function({ email, password }) {
+        const result = Joi.validate({ email, password }, schemas.authSchema);
+
+        return result;
+    }
+};

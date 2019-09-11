9993f5114b8c775c61801542c38dd0298766ff07
bd2187 <brandon.dionisio.26@gmail.com>
2019-09-10 19:59:34 -0700

    Import validateSignup fn from /userValidation.js. Use the fn to validate emails and passwords in the POST request of /signup

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const { validatSignup } = require("../utils/userValidation");
 
-router.route("/signup").post(function(req, res) {});
+router.route("/signup").post(function(req, res) {
+    const { email, password } = req.body;
+    const isValid = validatSignup({ email, password });
+
+    if (isValid.error) {
+        return res.status(400).json(isValid);
+    }
+
+    return res.json(isValid.value);
+});
 
 router.route("/signin").post(function(req, res) {});
 

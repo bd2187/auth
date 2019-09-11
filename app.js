b05e612441ba72997472cd25063e6804a953fe18
bd2187 <brandon.dionisio.26@gmail.com>
2019-09-10 21:13:59 -0700

    connect to db

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(morgan("dev"));
+// Connect to DB
+mongoose.connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("connected to db");
+});
 
-// parse application/x-www-form-urlencoded
+// Set up middleware
+app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
-// parse application/json
 app.use(bodyParser.json());
 
+// Routes
 app.use("/users", require("./routes/users"));
 
+// Listen to Port
 app.listen(PORT, () => {
     console.log(`server running on ${PORT}`);
 });

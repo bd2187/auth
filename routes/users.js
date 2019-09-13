1f3213b0932d63b260e4d30544d2aadd6869c437
bd2187 <brandon.dionisio.26@gmail.com>
2019-09-12 20:56:10 -0700

    Import User model. Save user to DB when in POST request of /signup

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,41 @@
 const express = require("express");
 const router = express.Router();
 const { validatSignup } = require("../utils/userValidation");
+const User = require("../models/user");
 
 router.route("/signup").post(function(req, res) {
-    const { email, password } = req.body;
+    let { email, password } = req.body;
+    email = email.toLowerCase();
     const isValid = validatSignup({ email, password });
 
     if (isValid.error) {
         return res.status(400).json(isValid);
     }
 
-    return res.json(isValid.value);
+    User.find({ email })
+        .then(function(data) {
+            if (data.length > 0) {
+                throw {
+                    message: `${email} already exists in db`
+                };
+            } else {
+                const newUser = new User({ email, password });
+                return newUser.save();
+            }
+        })
+        .then(function(data) {
+            if (data.email) {
+                return res.json({
+                    success: true,
+                    message: `Successfully saved ${email} to db`,
+                    data
+                });
+            }
+        })
+        .catch(function(err) {
+            err.success = false;
+            return res.status(400).json(err);
+        });
 });
 
 router.route("/signin").post(function(req, res) {});

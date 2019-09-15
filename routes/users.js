9791cfebc18a33e8cee4acd46abb62161c1e869b
bd2187 <brandon.dionisio.26@gmail.com>
2019-09-14 18:33:48 -0700

    Hash password before saving it to DB. Authenticate jwt token whenever user access /private-route

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,13 @@ const router = express.Router();
 const { validatSignup } = require("../utils/userValidation");
 const User = require("../models/user");
 const issueUserToken = require("../utils/issueUserToken");
+const passport = require("passport");
+const bcrypt = require("bcryptjs");
 
+/**
+ * Exchanges email and password for token
+ *
+ */
 router.route("/signup").post(function(req, res) {
     let { email, password } = req.body;
     email = email.toLowerCase();
@@ -15,17 +21,40 @@ router.route("/signup").post(function(req, res) {
 
     User.find({ email })
         .then(function(data) {
+            // Check if user exists in DB
             if (data.length > 0) {
                 throw {
                     message: `${email} already exists in db`
                 };
             } else {
-                const newUser = new User({ email, password });
+                // Generate salt to hash password
+                return bcrypt.genSalt(10);
+            }
+        })
+        .then(function(salt) {
+            if (salt) {
+                // hash user password
+                return bcrypt.hash(password, salt);
+            } else {
+                throw {
+                    message: "failed to generate salt for password"
+                };
+            }
+        })
+        .then(function(hash) {
+            if (hash) {
+                // save credentials to db
+                const newUser = new User({ email, password: hash });
                 return newUser.save();
+            } else {
+                throw {
+                    message: "failed to hash salt for password"
+                };
             }
         })
         .then(function(newUser) {
             if (newUser._id) {
+                // generate JWT to send back to client
                 return issueUserToken(newUser);
             } else {
                 throw {
@@ -53,9 +82,21 @@ router.route("/signup").post(function(req, res) {
             return res.status(400).json(err);
         });
 });
-
+/**
+ * Exchanges email and password for token
+ */
 router.route("/signin").post(function(req, res) {});
 
-router.route("/private-route").post(function(req, res) {});
+router
+    .route("/private-route")
+    .get(passport.authenticate("jwt", { session: false }), function(req, res) {
+        if (req.user) {
+            return res.json({ success: true, data: req.user });
+        } else {
+            return res
+                .status(403)
+                .json({ success: false, message: "forbidden path" });
+        }
+    });
 
 module.exports = router;

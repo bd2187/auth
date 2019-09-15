c94717123945827c326c9df7623b8c2b8fa7179c
bd2187 <brandon.dionisio.26@gmail.com>
2019-09-15 08:18:07 -0700

    Verify password in exchange for json webtoken in /signin route

--- routes/users.js
+++ routes/users.js
@@ -59,10 +59,56 @@ router.route("/signup").post(function(req, res) {
             return res.status(400).json(err);
         });
 });
+
 /**
  * Exchanges email and password for token
  */
-router.route("/signin").post(function(req, res) {});
+router.route("/signin").post(function(req, res) {
+    let { email, password } = req.body;
+    email = email.toLowerCase();
+    const isValid = validatSignup({ email, password });
+
+    if (isValid.error) {
+        return res.status(400).json(isValid);
+    }
+
+    var userObj = {};
+    User.findOne({ email })
+        .then(function(user) {
+            if (user) {
+                userObj = user;
+                return bcrypt.compare(password, user.password);
+            } else {
+                throw {
+                    message: `${email} not found`
+                };
+            }
+        })
+        .then(function(isPasswordValid) {
+            if (isPasswordValid) {
+                return issueUserToken(userObj);
+            } else {
+                throw {
+                    message: "incorrect password"
+                };
+            }
+        })
+        .then(function(token) {
+            if (token) {
+                return res.json({
+                    success: true,
+                    message: `logged in`,
+                    data: {
+                        token
+                    }
+                });
+            }
+        })
+        .catch(function(err) {
+            err.success = false;
+            return res.status(400).json(err);
+        });
+});
 
 router
     .route("/private-route")

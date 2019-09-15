089c4028e7cb51358d17562273693dc79f3faf8a
bd2187 <brandon.dionisio.26@gmail.com>
2019-09-14 18:31:16 -0700

    Initial commit. Create jwt and local passport middleware

---
+++ utils/passport.js
@@ -0,0 +1,41 @@
+const passport = require("passport");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const LocalStrategy = require("passport-local");
+const User = require("../models/user");
+
+// jwt strategy
+var opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken("authorization");
+opts.secretOrKey = process.env.JWT_SECRET;
+passport.use(
+    new JwtStrategy(opts, function(jwt_payload, done) {
+        User.findOne({ _id: jwt_payload.sub })
+            .then(function(user) {
+                return user ? done(null, user) : done(null, false);
+            })
+            .catch(function(err) {
+                if (err) {
+                    return done(err, false);
+                }
+            });
+    })
+);
+
+// local strategy
+passport.use(
+    new LocalStrategy(function(email, password, done) {
+        User.findOne({ email })
+            .then(function(user) {
+                if (!user || !user.verifyPassword(password))
+                    return done(null, false);
+
+                return done(null, user);
+            })
+            .catch(function(err) {
+                return done(err);
+            });
+    })
+);
+
+module.exports = passport;

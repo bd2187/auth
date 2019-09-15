d265f4ed031f1e1e2c544ef02e8aac91410e5ef7
bd2187 <brandon.dionisio.26@gmail.com>
2019-09-14 18:57:14 -0700

    Import bcrypt. Use userSchema's middleware when saving user to DB. Before saving user to DB, hash user password

--- models/user.js
+++ models/user.js
@@ -1,10 +1,25 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcryptjs");
 
 const userSchema = new Schema({
     email: { type: String, required: true, unique: true, lowercase: true },
     password: { type: String, required: true },
     date: { type: Date, require: true }
+});
+
+userSchema.pre("save", function(next) {
+    bcrypt
+        .genSalt(10)
+        .then(salt => {
+            if (salt) {
+                return bcrypt.hash(this.password, salt);
+            }
+        })
+        .then(hash => {
+            this.password = hash;
+            next();
+        });
 });
 
 const User = mongoose.model("User", userSchema);

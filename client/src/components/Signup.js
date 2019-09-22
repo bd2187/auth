9ec81a8055dff68f40972e058e3669118cc85c83
bd2187 <brandon.dionisio.26@gmail.com>
2019-09-22 14:16:58 -0700

    Create form to render inside of Signup component

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Signup = () => {
-    return <h1>signup</h1>;
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const updateInputValue = function(evt) {
+        var inputType = evt.target.getAttribute("type");
+        var inputValue = evt.target.value;
+
+        if (inputType === "email") {
+            setEmail(inputValue);
+        }
+
+        if (inputType === "password") {
+            setPassword(inputValue);
+        }
+    };
+
+    const signUpUser = function(evt) {
+        evt.preventDefault();
+    };
+
+    return (
+        <>
+            <form onSubmit={signUpUser}>
+                <div className="form-group">
+                    <input
+                        className="form-control"
+                        type="email"
+                        name="email"
+                        value={email}
+                        required
+                        onChange={updateInputValue}
+                        placeholder="email"
+                    />
+                </div>
+
+                <div className="form-group">
+                    <input
+                        className="form-control"
+                        type="password"
+                        name="password"
+                        value={password}
+                        required
+                        onChange={updateInputValue}
+                        placeholder="password"
+                    />
+                </div>
+                <input
+                    type="submit"
+                    value="Sign up"
+                    className="btn btn-primary"
+                />
+            </form>
+        </>
+    );
 };
 
 export default Signup;

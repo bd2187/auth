c5bff32d1f664f5659f68696f96619ed5d83eda7
bd2187 <brandon.dionisio.26@gmail.com>
2019-09-23 21:34:35 -0700

    Import SignUpContainer. Render the component for the /signup path

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,16 +4,18 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./Home";
 import Dashboard from "./Dashboard";
 import Signin from "./Signin";
-import Signup from "./Signup";
+import SignUpContainer from "../containers/SignUpContainer";
 
 const App = () => {
     return (
         <Router>
             <Header />
-            <Route exact path="/" component={Home} />
-            <Route path="/dashboard" component={Dashboard} />
-            <Route path="/signin" component={Signin} />
-            <Route path="/signup" component={Signup} />
+            <div className="container" style={{ marginTop: "50px" }}>
+                <Route exact path="/" component={Home} />
+                <Route path="/dashboard" component={Dashboard} />
+                <Route path="/signin" component={Signin} />
+                <Route path="/signup" component={SignUpContainer} />
+            </div>
         </Router>
     );
 };

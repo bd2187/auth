5ede73185c08781759a2623be1c82d7e4dcd6308
bd2187 <brandon.dionisio.26@gmail.com>
2019-09-23 21:32:56 -0700

    Initial commit. Create SignUpContainer which returns the Signup component

---
+++ client/src/containers/SignUpContainer.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
+import Signup from "../components/Signup";
+import { signUpUser } from "../actions/authActions";
+
+const SignUpContainer = props => {
+    if (props.isAuthenticated) {
+        return <Redirect to="/" />;
+    } else {
+        return <Signup signUpUser={props.signUpUser} error={props.error} />;
+    }
+};
+
+const mapStateToProps = state => {
+    return {
+        error: state.user.error,
+        isAuthenticated: state.user.isAuthenticated
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        signUpUser: (email, password) => dispatch(signUpUser(email, password))
+    };
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(SignUpContainer);

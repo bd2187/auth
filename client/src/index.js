dd904b337dab12b2b7da82115216a02cd840d7a4
bd2187 <brandon.dionisio.26@gmail.com>
2019-09-23 21:35:25 -0700

    Call applyMiddleware for thunk

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import * as serviceWorker from "./serviceWorker";
 import App from "./components/App";
-import combinedReducers from "./reducers";
+import thunk from "redux-thunk";
+import combinedReducers from "./reducers/reducers";
 
-const store = createStore(combinedReducers, {});
+const store = createStore(
+    combinedReducers,
+    {},
+    compose(
+        applyMiddleware(thunk),
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+            window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+);
 
 ReactDOM.render(
     <Provider store={store}>

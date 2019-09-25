dc5dc897f270ba5ad9e37347b9dad7e62435e866
bd2187 <brandon.dionisio.26@gmail.com>
2019-09-24 20:22:27 -0700

    Import HeaderContainer instead of Header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,17 @@
 import React from "react";
-import Header from "./Header";
+
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./Home";
 import Dashboard from "./Dashboard";
 import Signin from "./Signin";
+
+import HeaderContainer from "../containers/HeaderContainer";
 import SignUpContainer from "../containers/SignUpContainer";
 
 const App = () => {
     return (
         <Router>
-            <Header />
+            <HeaderContainer />
             <div className="container" style={{ marginTop: "50px" }}>
                 <Route exact path="/" component={Home} />
                 <Route path="/dashboard" component={Dashboard} />

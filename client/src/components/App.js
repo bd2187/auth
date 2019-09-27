946a129226d00fefe9377a7ecab8ff7e1f72521c
bd2187 <brandon.dionisio.26@gmail.com>
2019-09-26 21:22:31 -0700

    Import PrivateRouteContainer

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,22 +1,28 @@
 import React from "react";
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Home";
 import Dashboard from "./Dashboard";
-import Signin from "./Signin";
 
 import HeaderContainer from "../containers/HeaderContainer";
 import SignUpContainer from "../containers/SignUpContainer";
+import SignInContainer from "../containers/SignInContainer";
+import PrivateRouteContainer from "../containers/PrivateRouteContainer";
 
 const App = () => {
     return (
         <Router>
             <HeaderContainer />
             <div className="container" style={{ marginTop: "50px" }}>
-                <Route exact path="/" component={Home} />
-                <Route path="/dashboard" component={Dashboard} />
-                <Route path="/signin" component={Signin} />
-                <Route path="/signup" component={SignUpContainer} />
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/signin" component={SignInContainer} />
+                    <Route exact path="/signup" component={SignUpContainer} />
+                    <PrivateRouteContainer
+                        to="/dashboard"
+                        component={Dashboard}
+                    />
+                </Switch>
             </div>
         </Router>
     );

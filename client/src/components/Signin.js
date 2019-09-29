9213c8d4ae3888d1a69868383f5614f43fe5dfdf
bd2187 <brandon.dionisio.26@gmail.com>
2019-09-29 15:48:14 -0700

    Create form for user sign-in

--- client/src/components/Signin.js
+++ client/src/components/Signin.js
@@ -1,7 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Signin = () => {
-    return <h1>signin</h1>;
+const Signin = ({ signInUser, error }) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const updateInputValue = function(evt) {
+        var inputType = evt.target.getAttribute("type");
+        var inputValue = evt.target.value;
+
+        if (inputType === "email") {
+            setEmail(inputValue);
+        }
+
+        if (inputType === "password") {
+            setPassword(inputValue);
+        }
+    };
+
+    const submitForm = function(evt) {
+        evt.preventDefault();
+
+        signInUser(email, password);
+    };
+
+    return (
+        <>
+            <form onSubmit={submitForm}>
+                <div className="form-group">
+                    <input
+                        className="form-control"
+                        type="email"
+                        name="email"
+                        value={email}
+                        required
+                        onChange={updateInputValue}
+                        placeholder="email"
+                    />
+                </div>
+
+                <div className="form-group">
+                    <input
+                        className="form-control"
+                        type="password"
+                        name="password"
+                        value={password}
+                        required
+                        onChange={updateInputValue}
+                        placeholder="password"
+                    />
+                </div>
+
+                {error.message ? (
+                    <div className="alert alert-danger">{error.message}</div>
+                ) : null}
+
+                <input
+                    type="submit"
+                    value="Sign in"
+                    className="btn btn-primary"
+                />
+            </form>
+        </>
+    );
 };
 
 export default Signin;

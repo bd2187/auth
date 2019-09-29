bdf03095778e2da77df787110a613ffb92345bc7
bd2187 <brandon.dionisio.26@gmail.com>
2019-09-29 15:48:45 -0700

    Add button to make request to private route

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,24 @@
 import React from "react";
+import axios from "axios";
+
+const makeRequest = () => {
+    axios
+        .get("http://localhost:5000/users/private-route")
+        .then(res => {
+            console.warn(res);
+        })
+        .catch(err => {
+            console.warn(err);
+        });
+};
 
 const Home = () => {
-    return <h1>home</h1>;
+    return (
+        <div>
+            <h1>home</h1>
+            <button onClick={makeRequest}>request</button>
+        </div>
+    );
 };
 
 export default Home;

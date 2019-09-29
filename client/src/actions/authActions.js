02285d1ef17994ed924d3f014548a40e379a6be2
bd2187 <brandon.dionisio.26@gmail.com>
2019-09-29 15:50:42 -0700

    Import setAuthHeader(). Call the fn inside of signUpUser(), signInUser(), and signOutUser().

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import setAuthHeader from "../utils/setAuthHeader";
 
 export function signUpUser(email, password) {
     return function(dispatch) {
@@ -16,6 +17,7 @@ export function signUpUser(email, password) {
                     });
 
                     localStorage.setItem("token", res.data.data.token);
+                    setAuthHeader(res.data.data.token);
                 } else {
                     dispatch({
                         type: "AUTH_ERROR",
@@ -32,10 +34,42 @@ export function signUpUser(email, password) {
     };
 }
 
-export function signInUser() {}
+export function signInUser(email, password) {
+    return function(dispatch) {
+        dispatch({
+            type: "LOAD_AUTHENTICATION"
+        });
+
+        return axios
+            .post("http://localhost:5000/users/signin", { email, password })
+            .then(res => {
+                if (res.data.success) {
+                    dispatch({
+                        type: "SIGN_IN_USER",
+                        payload: res.data
+                    });
+
+                    localStorage.setItem("token", res.data.data.token);
+                    setAuthHeader(res.data.data.token);
+                } else {
+                    dispatch({
+                        type: "AUTH_ERROR",
+                        payload: res.data
+                    });
+                }
+            })
+            .catch(err => {
+                dispatch({
+                    type: "AUTH_ERROR",
+                    payload: err
+                });
+            });
+    };
+}
 
 export function signOutUser() {
     localStorage.removeItem("token");
+    setAuthHeader(null);
 
     return { type: "SIGN_OUT_USER", payload: "" };
 }
